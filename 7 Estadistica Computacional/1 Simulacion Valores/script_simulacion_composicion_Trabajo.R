c4abd157b3cc54bfefe90c4b1565ec708213b8d3
khaquehua <kevinhaquehua@gmail.com>
2025-05-16 20:38:09 -0500

    Modelo de composición

--- 7 Estadistica Computacional/1 Simulacion Valores/script_simulacion_composicion_Trabajo.R
+++ 7 Estadistica Computacional/1 Simulacion Valores/script_simulacion_composicion_Trabajo.R
@@ -1,10 +1,11 @@
 ## Simulación de valores de variable aleatoria por el método de la composición
+# f(x) = 0.3 N(0,1) + 0.7 Exp (1)
+# Kevin Haquehua Apaza
 set.seed(123)
-
-n <- 100
+n <- 1000
 # Probabilidades de mezcla
-p1 <- 0.6
-p2 <- 0.4
+p1 <- 0.3
+p2 <- 0.7
 
 # paso 1: Generar la variable auxiliar para identificar a qué distribución pertenece
 I <- sample(c(1,2), size = n, replace = TRUE, prob = c(p1,p2))
@@ -19,13 +20,16 @@ for (i in 1:n) {
   if (I[i] == 1){
     muestra[i] <- rnorm(1, mean = 0, sd = 1)
   } else { ## I[i] == 2
-    muestra[i] <- rnorm(1, mean = 3, sd = 2)
+    muestra[i] <- rexp(1, rate = 1)
   }
 }
 
 muestra
 
 ## Histograma de los valores simulados
-hist(muestra, breaks = 20, probability = TRUE)
-summary(muestra)
+hist(muestra, breaks = 100, probability = TRUE,
+     xlab = "Muestra con valores simulados de X",
+     ylab = "Densidad de X",
+     xlim = c(-2,5),
+     col = "green")
 boxplot(muestra, col = "skyblue")
