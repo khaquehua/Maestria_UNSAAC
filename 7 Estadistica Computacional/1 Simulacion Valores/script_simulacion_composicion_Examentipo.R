556022aabe73888b1fc09624ba4890e505c28357
khaquehua <kevinhaquehua@gmail.com>
2025-05-16 20:47:40 -0500

    Subir datos de la simulacion por composicion tipo examen

--- 7 Estadistica Computacional/1 Simulacion Valores/script_simulacion_composicion_Examentipo.R
+++ 7 Estadistica Computacional/1 Simulacion Valores/script_simulacion_composicion_Examentipo.R
@@ -1,14 +1,15 @@
 ## Simulación de valores de variable aleatoria por el método de la composición
-# f(x) = 0.3 N(0,1) + 0.7 Exp (1)
+# f(x) = 0.3 N(2,1) + 0.5 Exp (3.5) + 0.2 Exp (1.5)
 # Kevin Haquehua Apaza
 set.seed(123)
 n <- 1000
 # Probabilidades de mezcla
 p1 <- 0.3
-p2 <- 0.7
+p2 <- 0.5
+p3 <- 0.2
 
 # paso 1: Generar la variable auxiliar para identificar a qué distribución pertenece
-I <- sample(c(1,2), size = n, replace = TRUE, prob = c(p1,p2))
+I <- sample(c(1,2,3), size = n, replace = TRUE, prob = c(p1,p2,p3))
 I
 
 # paso 2: Crear un vector vacío llamado muestra
@@ -18,9 +19,13 @@ muestra
 # paso 3: Simular los valores para la nueva distribución
 for (i in 1:n) {
   if (I[i] == 1){
-    muestra[i] <- rnorm(1, mean = 0, sd = 1)
-  } else { ## I[i] == 2
-    muestra[i] <- rexp(1, rate = 1)
+    muestra[i] <- rnorm(1, mean = 2, sd = 1)
+  } else { 
+    if (I[i] == 2) { ## I[i] == 2
+      muestra[i] <- rexp(1, rate = 3.5)
+    } else { ## I[i] == 3
+      muestra[i] <- rexp(1, rate = 1.5)
+    }
   }
 }
 
@@ -30,6 +35,5 @@ muestra
 hist(muestra, breaks = 100, probability = TRUE,
      xlab = "Muestra con valores simulados de X",
      ylab = "Densidad de X",
-     xlim = c(-2,5),
      col = "green")
 boxplot(muestra, col = "skyblue")
