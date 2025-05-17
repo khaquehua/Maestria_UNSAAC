769bbe564df3f5c7b5f047ee251d8bdca965d3d0
khaquehua <kevinhaquehua@gmail.com>
2025-05-16 20:24:40 -0500

    Metodo de composicion simulacion

--- 7 Estadistica Computacional/1 Simulacion Valores/script_simulacion_composicion.R
+++ 7 Estadistica Computacional/1 Simulacion Valores/script_simulacion_composicion.R
@@ -1,153 +1,31 @@
-### Simular valores de Variable Aleatoria por el método de aceptación-rechazo
+## Simulación de valores de variable aleatoria por el método de la composición
+set.seed(123)
 
-## Generando la función para simular los valores de la variable aleatoria por el método
-## de aceptación-rechazo
+n <- 100
+# Probabilidades de mezcla
+p1 <- 0.6
+p2 <- 0.4
 
-metodo_ar <- function(nsims){
-  muestra <- numeric(nsims)
-  aceptado <- 0
-  intento <- 0
-  while (aceptado < nsims) {
-    x <- runif(1, 0, 1) # Valor de la función propuesta g(x), es una Uniforme [0,1]
-    u <- runif(1, 0, 1) # Valor de la uniforme para comparar 
-    
-    if (u <= x) { #Criterio de aceptación (f(x)/c*g(x)) = 2x/2  = x
-      aceptado <- aceptado + 1
-      muestra[aceptado] <- x
-    }
-    intento <- intento + 1
+# paso 1: Generar la variable auxiliar para identificar a qué distribución pertenece
+I <- sample(c(1,2), size = n, replace = TRUE, prob = c(p1,p2))
+I
+
+# paso 2: Crear un vector vacío llamado muestra
+muestra <- numeric(n)
+muestra
+
+# paso 3: Simular los valores para la nueva distribución
+for (i in 1:n) {
+  if (I[i] == 1){
+    muestra[i] <- rnorm(1, mean = 0, sd = 1)
+  } else { ## I[i] == 2
+    muestra[i] <- rnorm(1, mean = 3, sd = 2)
   }
-  
-  return(muestra)
 }
 
-## Generar 100 valores
-set.seed(12005)
-muestra <- metodo_ar(100)
 muestra
 
 ## Histograma de los valores simulados
-hist(muestra, probability = TRUE, breaks = 25, 
-     xlab = "Muestra con valores simulados de X",
-     ylab = "Densidad de X",
-     xlim = c(0,1), ylim = c(0, 3.5),
-     col = "green")
-curve(2*x, from = 0, to = 1, lwd=3, col = "blue", add = TRUE)
-legend("topright",legend = c("Función de densidad: 2x","Histograma"), col = c("blue","green"), lwd =3)
-
-## Interpreten el histograma para 5000 valores y después generar
-## Una muestra con 5000 valores
-
-## Generar 5000 valores
-set.seed(12005)
-muestra <- metodo_ar(5000)
-muestra
-
-## Histograma de los valores simulados
-hist(muestra, probability = TRUE, breaks = 25, 
-     xlab = "Muestra con valores simulados de X",
-     ylab = "Densidad de X",
-     xlim = c(0,1), ylim = c(0, 3.5),
-     col = "green")
-curve(2*x, from = 0, to = 1, lwd=3, col = "blue", add = TRUE)
-legend("topright",legend = c("Función de densidad: 2x","Histograma"), col = c("blue","green"), lwd =3)
-
-
-### Simular valores de Variable Aleatoria por el método de aceptación-rechazo
-
-## Generando la función para simular los valores de la variable aleatoria por el método
-## de aceptación-rechazo
-
-metodo_ar <- function(nsims){
-  muestra <- numeric(nsims)
-  aceptado <- 0
-  intento <- 0
-  while (aceptado < nsims) {
-    x <- runif(1, 0, 1) # Valor de la función propuesta g(x), es una Uniforme [0,1]
-    u <- runif(1, 0, 1) # Valor de la uniforme para comparar 
-    
-    if (u <= x^2) { #Criterio de aceptación (f(x)/c*g(x)) = 3x^2/3  = x
-      aceptado <- aceptado + 1
-      muestra[aceptado] <- x
-    }
-    intento <- intento + 1
-  }
-  
-  return(muestra)
-}
-
-## Generar 100 valores
-set.seed(12005)
-muestra <- metodo_ar(100)
-muestra
-
-## Histograma de los valores simulados
-hist(muestra, probability = TRUE, breaks = 25, 
-     xlab = "Muestra con valores simulados de X",
-     ylab = "Densidad de X",
-     xlim = c(0,1), ylim = c(0, 3.5),
-     col = "green")
-curve(3*x^2, from = 0, to = 1, lwd=3, col = "blue", add = TRUE)
-legend("topright",legend = c("Función de densidad: 2x","Histograma"), col = c("blue","green"), lwd =3)
-
-## Interpreten el histograma para 5000 valores y después generar
-## Una muestra con 5000 valores
-
-## Generar 5000 valores
-set.seed(12005)
-muestra <- metodo_ar(1000000)
-muestra
-
-## Histograma de los valores simulados
-hist(muestra, probability = TRUE, breaks = 100, 
-     xlab = "Muestra con valores simulados de X",
-     ylab = "Densidad de X",
-     xlim = c(0,1), ylim = c(0, 3.5),
-     col = "green")
-curve(3*x^2, from = 0, to = 1, lwd=3, col = "blue", add = TRUE)
-legend("topright",legend = c("Función de densidad: 3x^2","Histograma"), col = c("blue","green"), lwd =3)
+hist(muestra, breaks = 20, probability = TRUE)
+summary(muestra)
 boxplot(muestra, col = "skyblue")
-
-
-
-
-### Simular valores de Variable Aleatoria por el método de aceptación-rechazo
-
-# 1. Generar dos numeros aleatorios U_1, U_2.
-set.seed(3)
-u1 <- runif(1)
-u2 <- runif(1)
-
-# 2. Comparar con f(x)/cg(x)
-while(u2>(256/27)*u1*(1-u1)^3){
-  u1 <- runif(1)
-  u2 <- runif(1)
-}
-X=u1
-X
-
-
-ddensidad<- function(x){
-  20*x*(1-x)^3
-}
-## Creamos una funcion para simular los valores de la variable
-beta20 <- function(){
-  # 1. Generar dos numeros aleatorios U_1, U_2.
-  u1 <- runif(1)
-  u2 <- runif(1)
-  # 2. Comparar con f(x)/cg(x)
-  while(u2 > (256 / 27) * u1 * (1 - u1) ^ 3){
-    u1 <- runif(1)
-    u2 <- runif(1)
-  }
-  u1
-}
-library(dplyr)
-library(plyr)
-sims <- rdply(10000, beta20)
-sims
-
-
-hist(sims$V1, breaks="FD", freq=FALSE,
-     xlim = c(0,1),ylim = c(0,2.5),main = "Histograma para valores simulados")
-curve(ddensidad(x), add=TRUE, col="red")
