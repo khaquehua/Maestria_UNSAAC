a050fdcd27359ff850e3c225cccc194e9c413e1d
khaquehua <kevinhaquehua@gmail.com>
2025-09-06 10:11:48 -0500

    Culminacion script introduccion

--- 11 Diseño Experimentos/1 Introduccion/script_introduccion.R
+++ 11 Diseño Experimentos/1 Introduccion/script_introduccion.R
@@ -11,3 +11,21 @@ boxplot(volumen~maquina,col = "gold")
 # se observa que no hay diferencias significativa
 
 library(Rcmdr)
+plotMeans(volumen, maquina, error.bars = "conf.int")
+
+# Prueba de medias independintes
+# Prueba de normalidad
+shapiro.test(volumen[1:10]) # Los datos provienen de una dist. normal
+shapiro.test(volumen[11:20]) # Los datos provienen de una dist. normal
+
+bartlett.test(volumen~maquina)
+
+# Prueba de comparacion de dos medias
+# H0: mu_1 = mu_2
+# H0: mu_1 != mu_2
+
+t.test(volumen~maquina, alternative = "t", var.equal = T)
+
+# Como el pvalor = 0.4347 > 0.05, no se rechaza H0 
+# En promedio el llenado (volumen) es similar para
+# ambas maquinas
